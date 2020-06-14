6d328d639201a0dc3c2e0423a106577773f288c2
Your Name <you@example.com>
2020-06-14 06:43:04 -0700

    Javascript Assignment

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
 //**** Chatpter # 1 *****//
 
-//Task No. 1
+//____________Task No. 1___________
 //alert("Hello Dear, Welcome!")
-//Task No. 2
+//____________Task No. 2___________
 //alert( "Error! Please enter a valid password")
-//Task No.3
+//____________Task No.3____________
 //alert("Welcome to Js Land \n Happy Coding ..")
-//Task No. 4
+//____________Task No. 4___________
 //alert("Hello I can run this from my browser console");
-//Task No. 5
+//____________Task No. 5___________
 //var a = alert("Hello I can run my JS through web browser console");
 //console.log(a);
 
 //****** Chapter # 2 *******//
-//Task No. 1
+
+//____________Task No. 1___________
 //var username= "Sheeza";
 //console.log(username)
-//Task No. 2
+//____________Task No. 2___________
 //var myName="Sheeza Raees"
 //console.log(myName)
-//Task No. 3
+//___________Task No. 3_____________
 //var message="Hello World";
 //alert(message);
+//___________Task No. 4_____________
+//alert("Jhone Doe");
+//alert("15 years old");
+//alert("Certified Mobile Application Development")
+//___________Task No. 5______________
+//var a="PIZZA \n PIZZ \n PIZ \n PI \n P"
+//alert(a)
+//_____________Task No.6______________
+//var email="rsyeadsheeza@gmail.com"
+//alert("My email address is" +" " + email);
+//_____________Task No. 7______________
+//var a="Yah! I can write html content through Javascript";
+//document.write(a);
+//_____________Task No. 8______________
+//var a="▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(a);
+//document.write(a);
+
+//******* Chapter # 3 **********//
+
+//____________Task No. 1___________
+//var age=15;
+//alert("I am " + "" + age + " " + "years old")
+//____________Task No. 2____________
+//for( var a=1; a<11 ; a++){
+   // var b= a;
+    //alert("you have visited" + " " + b+ " " + "times");
+    
+
+//}
+//____________Task No. 3_____________
+document.write("alpha")
+var birthYear=1998;
+document.write("My birth year is "+ " " + birth Year);
+document.write("Data type of my declared variable is number");
