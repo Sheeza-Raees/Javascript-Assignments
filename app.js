00ed2fc52c0e92964e45952e4878cce113dd7ed1
Your Name <you@example.com>
2020-06-14 05:19:41 -0700

    Javascript's Assignment

---
+++ app.js
@@ -0,0 +1,16 @@
+//**** Chatpter # 1 *****//
+
+//Task No. 1
+//alert("Hello Dear, Welcome!")
+//Task No. 2
+//alert( "Error! Please enter a valid password")
+//Task No.3
+//alert("Welcome to Js Land \n Happy Coding ..")
+//Task No. 4
+//alert("Hello I can run this from my browser console");
+//Task No. 5
+//var a = alert("Hello I can run my JS through web browser console");
+//console.log(a);
+
+//****** Chapter # 2 *******//
+//Task No. 1
